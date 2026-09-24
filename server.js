bac356a2f622e7c2586868fac227457950733363
Mohamed amine TELJAOUI <93794412+MoAmiine@users.noreply.github.com>
2026-09-24 16:52:17 +0000

    Refactor routing to use withParams for better parameter handling and add registration routes

--- server.js
+++ server.js
@@ -13,41 +13,50 @@ const associationController = require('./src/controllers/associationController')
 const activityController = require('./src/controllers/activityController');
 const memberController = require('./src/controllers/memberController');
 const familyController = require('./src/controllers/familyController');
+const registrationController = require('./src/controllers/registrationController');
 
 const serve = serveStatic(path.join(__dirname, 'public'));
 const parseFormBody = bodyParser.urlencoded({ extended: false });
+
+  function withParams(handler) {
+  return (req, res, params) => {
+    req.params = params;
+    return handler(req, res, params);
+  };
+}
 
 router.on('GET', '/', (req, res) => {
   render(res, 'dashboard', { title: 'Tableau de bord' });
 });
 
 router.on('GET', '/facilities', facilityController.listFacilities);
-router.on('GET', '/facilities/:id/edit', facilityController.showEditForm);
+router.on('GET', '/facilities/:id/edit', withParams(facilityController.showEditForm));
 router.on('POST', '/facilities', facilityController.createFacility);
-router.on('POST', '/facilities/:id/update', facilityController.updateFacility);
-router.on('POST', '/facilities/:id/delete', facilityController.deleteFacility);
+router.on('POST', '/facilities/:id/update', withParams(facilityController.updateFacility));
+router.on('POST', '/facilities/:id/delete', withParams(facilityController.deleteFacility));
 
 router.on('GET', '/associations', associationController.listAssociations);
-router.on('GET', '/associations/:id/edit', associationController.showEditForm);
+router.on('GET', '/associations/:id/edit', withParams(associationController.showEditForm));
 router.on('POST', '/associations', associationController.createAssociation);
-router.on('POST', '/associations/:id/update', associationController.updateAssociation);
-router.on('POST', '/associations/:id/delete', associationController.deleteAssociation);
+router.on('POST', '/associations/:id/update', withParams(associationController.updateAssociation));
+router.on('POST', '/associations/:id/delete', withParams(associationController.deleteAssociation));
 
 router.on('GET', '/activities', activityController.listActivities);
 router.on('POST', '/activities', activityController.createActivity);
-router.on('POST', '/activities/:id/delete', activityController.deleteActivity);
-
+router.on('POST', '/activities/:id/delete', withParams(activityController.deleteActivity));
+router.on('GET', '/activities/:id', withParams(registrationController.showActivityDetail));
+router.on('POST', '/registrations/quote', registrationController.showQuote);
+router.on('POST', '/registrations', registrationController.confirmRegistration);
 
 router.on('GET', '/members', memberController.listMembers);
 router.on('POST', '/members', memberController.createMember);
-router.on('POST', '/members/:id/delete', memberController.deleteMember);
+router.on('POST', '/members/:id/delete', withParams(memberController.deleteMember));
 
 router.on('GET', '/families', familyController.listFamilies);
-router.on('GET', '/families/:id/edit', familyController.showEditForm);
+router.on('GET', '/families/:id/edit', withParams(familyController.showEditForm));
 router.on('POST', '/families', familyController.createFamily);
-router.on('POST', '/families/:id/update', familyController.updateFamily);
-router.on('POST', '/families/:id/delete', familyController.deleteFamily);
-
+router.on('POST', '/families/:id/update', withParams(familyController.updateFamily));
+router.on('POST', '/families/:id/delete', withParams(familyController.deleteFamily));
 
 const server = http.createServer((req, res) => {
   serve(req, res, (err) => {
@@ -63,5 +72,5 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`server started : http://localhost:${PORT}`);
+  console.log(`Serveur démarré : http://localhost:${PORT}`);
 });
